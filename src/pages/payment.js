1002595e34ec1bd397fa5420e2789cd53a8eda4f
AmbalikaSikarwar <ambalikayuvasoft355@gmail.com>
2023-10-21 12:23:56 +0530

    new commit

--- src/pages/payment.js
+++ src/pages/payment.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Payment() {
   return (
     <div className='payment'>
-      <h1>welcome to payment page</h1>
+      <h1>welcome to payment page ....</h1>
     </div>
   );
 }
